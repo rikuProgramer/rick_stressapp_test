80beae70d864a2127b368c9815b2b2e5c21daa5b
rikuProgramer <rick722@icloud.com>
2022-05-05 23:55:57 +0900

    日記を書く際の画面Nikki_text_add.dartのUIを変更しました。 より見やすく整理されたと思います。 具体的には 	感情を選択できるようになった 	appbarにカレンダーが表示されるようになりbodyからは削除された
    次回は感情を選択した際にcancelをタップすること無く選択した時点で自動的に戻るようにしたいと思います。これはpopさせればよいだけなのですぐに実装できると思います。

--- lib/Emotion_radio_button.dart
+++ lib/Emotion_radio_button.dart
@@ -12,36 +12,16 @@ class EmotionRadioButton extends StatefulWidget {
 
 class _EmotionRadioButtonState extends State<EmotionRadioButton> {
   SortOfEmotions? _character = SortOfEmotions.Happy;
-
+  final items = ['happy', 'angry' 'sad', 'fun'];
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        ListTile(
-          title: const Text('Happy'),
-          leading: Radio<SortOfEmotions>(
-            value: SortOfEmotions.Happy,
-            groupValue: _character,
-            onChanged: (SortOfEmotions? value) {
-              setState(() {
-                _character = value;
-              });
-            },
-          ),
-        ),
-        ListTile(
-          title: const Text('Angry'),
-          leading: Radio<SortOfEmotions>(
-            value: SortOfEmotions.Angry,
-            groupValue: _character,
-            onChanged: (SortOfEmotions? value) {
-              setState(() {
-                _character = value;
-              });
-            },
-          ),
-        ),
-      ],
+    return Scaffold(
+        body: CupertinoAlertDialog(
+          title: Text('感情を教えて下さい'),
+          actions: [
+
+          ],
+        )
     );
   }
 }
--- lib/Nikki_text_app.dart
+++ lib/Nikki_text_app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
 import 'Emotion_radio_button.dart';
 
 class TextAddPage extends StatefulWidget {
@@ -7,7 +8,6 @@ class TextAddPage extends StatefulWidget {
 
   @override
   _TextAddPageState createState() => _TextAddPageState();
-
 }
 
 class _TextAddPageState extends State<TextAddPage> {
@@ -19,13 +19,30 @@ class _TextAddPageState extends State<TextAddPage> {
   final TextEditingController _controller = TextEditingController();
   final TextEditingController _titleController = TextEditingController();
   final textController = TextEditingController();
-  var todayDate = '日付を入力してください';
+  var todayDate = '今日の日付を教えて下さい';
+
+
+  //sort of emotions
+  //感情を選択する前は「ノーマル」に設定されている。
+  var userEmotion = 'https://1.bp.blogspot.com/-OwZV0x1FJrs/UNQrdb_uppI/AAAAAAAAI7U/-iJhiSxVI7Q/s1600/mark_face_smile.png';
+  final normal =
+      'https://1.bp.blogspot.com/-OwZV0x1FJrs/UNQrdb_uppI/AAAAAAAAI7U/-iJhiSxVI7Q/s1600/mark_face_smile.png';
+  final sadness =
+      'https://3.bp.blogspot.com/--qQ5wmWDm9k/UNQrZ_BB5iI/AAAAAAAAI6s/IoaUHEtRpzE/s1600/mark_face_cry.png';
+  final anger =
+      'https://3.bp.blogspot.com/-tSzqnc5n4ak/UNQrWx6BLrI/AAAAAAAAI6g/wvAwk_K1Avg/s1600/mark_face_angry.png';
+  final fear =
+      'https://3.bp.blogspot.com/-TnPYwXbLqzo/UNQrbXtPyoI/AAAAAAAAI68/5FsIRIvRjf0/s1600/mark_face_jito.png';
+  final disgust =
+      'https://2.bp.blogspot.com/-mfrs0Hl-K4Y/UNQrXi-WBMI/AAAAAAAAI6o/w4hAwmqU78U/s1600/mark_face_ase.png';
+  final happyness =
+      'https://4.bp.blogspot.com/-QeM2lPMumuo/UNQrby-TEPI/AAAAAAAAI7E/cZIpq3TTyas/s1600/mark_face_laugh.png';
+
 
 
   Future _pickDate(BuildContext context) async {
     //DatePickerの初期値
     final nowDate = DateTime.now();
-
     //DatePickerを表示し、選択されたら変数に格納する
     final newDate = await showDatePicker(
         context: context,
@@ -37,10 +54,7 @@ class _TextAddPageState extends State<TextAddPage> {
             .year - 10),
         lastDate: DateTime(DateTime
             .now()
-            .year + 1)
-    );
-
-
+            .year + 1));
     //nullチェック
     if (newDate != null) {
       //選択された日付を変数に格納
@@ -57,145 +71,252 @@ class _TextAddPageState extends State<TextAddPage> {
     }
   }
 
-    @override
-    void dispese() {
-      textController.dispose();
-      super.dispose();
-    }
+  @override
+  void dispese() {
+    textController.dispose();
+    super.dispose();
+  }
 
+  @override
+  Widget build(BuildContext context) {
+    final double deviceWidth = MediaQuery
+        .of(context)
+        .size
+        .width;
 
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        appBar: AppBar(
-          title: const Text('日記を作成'),
-        ),
-        body: Column(
-          children: <Widget>[
-            Container(
-              color: Colors.white30,
-              width: double.infinity,
-              child: TextField(
-                controller: _titleController,
-                maxLines: 1,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: '日記のタイトルを入力',
-                ),
-                onChanged: (String valueTitle) {
-                  setState(() {
-                    _title = valueTitle;
-                  });
-                },
-              ),
-            ),
-            ElevatedButton(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(todayDate),
+        actions: [
+          IconButton(
               onPressed: () {
-                ///カレンダーを起動させる1行
                 _pickDate(context);
               },
-              child: Text(todayDate),
-            ),
+              icon: const Icon(Icons.arrow_drop_down_sharp))
+        ],
+      ),
+      body: Column(
+        children: <Widget>[
 
-            ///ここはRadioButtonを呼び出します→Emotion_radio_button.dart
-            ///意外とスペースを圧迫するので他のやり方を試したいと思います。
-            ElevatedButton(
-              onPressed: () {
-                EmotionRadioButton();
-              },
-              child: const Text('感情を記録する'),
-
-            ),
-
-
-            Container(
-              color: const Color.fromRGBO(255, 204, 229, 180),
-              width: double.infinity,
-              child: TextField(
-                controller: textController,
-                keyboardType: TextInputType.number,
-                decoration: const InputDecoration(
-                  labelText: '0~10でストレスの強さを教えて下さい',
-                  hintText: '例: 10→過去最高のストレス',
-                  icon: Icon(Icons.favorite),
-                  fillColor: Colors.white,
+          Row(
+            children: <Widget>[
+              Container(
+                width: deviceWidth * 0.8,
+                color: Colors.white30,
+                child: TextField(
+                  controller: _titleController,
+                  maxLines: 1,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: '日記のタイトルを入力',
+                  ),
+                  onChanged: (String valueTitle) {
+                    setState(() {
+                      _title = valueTitle;
+                    });
+                  },
                 ),
-                onChanged: (String inputStress) {
-                  setState(() {
-                    stress = inputStress as int;
-                    //int.parse(stress);
-                  });
-                },
               ),
-            ),
-            Container(
-              color: Colors.white,
-              width: double.infinity,
-              child: TextField(
-                controller: _controller,
-                keyboardType: TextInputType.multiline,
-                maxLines: null,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: '今日の話を聞かせてください',
-                  hintText: '雨が降っていて憂鬱だった。',
-                ),
-                onChanged: (String valueText) {
-                  // データが変更したことを知らせる（画面を更新する）
-                  setState(() {
-                    // データを変更
-                    _text = valueText;
-                  });
-                },
-              ),
-            ),
-
-            const SizedBox(height: 5),
-
-            ///この上下のsizedBoxってどう違うの？
-            SizedBox(
-              width: double.infinity,
-              child: TextButton(
-                onPressed: () =>
-                    showDialog<String>(
+              Container(
+                width: deviceWidth * 0.2,
+                child: GestureDetector(
+                  child: Image.network(
+                    userEmotion
+                  ),
+                  onTap: () {
+                    showDialog(
                       context: context,
-                      builder: (BuildContext context) =>
-                          AlertDialog(
-                            title: const Text('本当に日記を破棄してもよろしいですか？'),
-                            actions: <Widget>[
-                              TextButton(
-                                onPressed: () =>
-                                    Navigator.pop(context, 'Cancel'),
-                                child: const Text('日記を続ける'),
+                      barrierDismissible: false,
+                      builder: (_) {
+                        return AlertDialog(
+                          insetPadding: const EdgeInsets.all(8),
+                          title: const Text('今の感情を教えて下さい'),
+                          content: Row(
+                            children: <Widget>[
+                              Column(
+                                children: [
+                                  SizedBox(
+                                    child: GestureDetector(
+                                      child: Image.network(normal),
+                                      onTap: () {
+                                        setState(() {
+                                          userEmotion = normal;
+                                        });
+                                      },
+                                    ),
+                                    width: 100,
+                                  ),
+                                  SizedBox(
+                                    child: GestureDetector(
+                                      child: Image.network(happyness),
+                                      onTap: () {
+                                        setState(() {
+                                          userEmotion = happyness;
+                                        });
+                                      },
+                                    ),
+                                    width: 100,
+                                  ),
+                                ],
                               ),
-                              TextButton(
-                                onPressed: () {
-                                  Navigator.of(context).popUntil((
-                                      route) => route.isFirst);
-                                },
-                                child: const Text('破棄する'),
+                              Column(
+                                children: [
+                                  SizedBox(
+                                    child: GestureDetector(
+                                      child: Image.network(anger),
+                                      onTap: () {
+                                        setState(() {
+                                          userEmotion = anger;
+                                        });
+                                      },
+                                    ),
+                                    width: 100,
+                                  ),
+                                  SizedBox(
+                                    child: GestureDetector(
+                                      child: Image.network(sadness),
+                                      onTap: () {
+                                        setState(() {
+                                          userEmotion = sadness;
+                                        });
+                                      },
+                                    ),
+                                    width: 100,
+                                  ),
+                                ],
                               ),
+                              Column(
+                                children: [
+                                  SizedBox(
+                                    child: GestureDetector(
+                                      child: Image.network(fear),
+                                      onTap: () {
+                                        setState(() {
+                                          userEmotion = fear;
+                                        });
+                                      },
+                                    ),
+                                    width: 100,
+                                  ),
+                                  SizedBox(
+                                    child: GestureDetector(
+                                      child: Image.network(disgust),
+                                      onTap: () {
+                                        setState(() {
+                                          userEmotion = disgust;
+                                        });
+                                      },
+                                    ),
+                                    width: 100,
+                                  ),
+                                ],
+                              )
                             ],
                           ),
-                    ),
-                child: const Text('日記を破棄する'),
+                          actions: [
+                            ElevatedButton(
+                              child: const Text("Cancel"),
+                              onPressed: () => Navigator.pop(context),
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  },
+                ),
               ),
+            ],
+          ),
+
+          Container(
+            color: const Color.fromRGBO(255, 204, 229, 180),
+            width: double.infinity,
+            child: TextField(
+              controller: textController,
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                labelText: '0~10でストレスの強さを教えて下さい',
+                hintText: '例: 10→過去最高のストレス',
+                icon: Icon(Icons.favorite),
+                fillColor: Colors.white,
+              ),
+              onChanged: (String inputStress) {
+                setState(() {
+                  stress = inputStress as int;
+                  //int.parse(stress);
+                });
+              },
             ),
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: const Text('作成'),
-          onPressed: () async {
-            print(_controller.text);
-            print(_text);
-            print(selectedDate);
-            print(stress);
-            if (stress is int) {
-              print('stress はint型です');
-            };
-            Navigator.of(context).pop(_titleController.text);
-          },
-        ),
-      );
-    }
+          ),
+
+          Container(
+            color: Colors.white,
+            width: double.infinity,
+            child: TextField(
+              controller: _controller,
+              keyboardType: TextInputType.multiline,
+              maxLines: null,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: '今日の話を聞かせてください',
+                hintText: '雨が降っていて憂鬱だった。',
+              ),
+              onChanged: (String valueText) {
+                // データが変更したことを知らせる（画面を更新する）
+                setState(() {
+                  // データを変更
+                  _text = valueText;
+                });
+              },
+            ),
+          ),
+
+          const SizedBox(height: 5),
+
+          ///この上下のsizedBoxってどう違うの？
+          SizedBox(
+            width: double.infinity,
+            child: TextButton(
+              onPressed: () =>
+                  showDialog<String>(
+                    context: context,
+                    builder: (BuildContext context) =>
+                        AlertDialog(
+                          title: const Text('本当に日記を破棄してもよろしいですか？'),
+                          actions: <Widget>[
+                            TextButton(
+                              onPressed: () => Navigator.pop(context, 'Cancel'),
+                              child: const Text('日記を続ける'),
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(context)
+                                    .popUntil((route) => route.isFirst);
+                              },
+                              child: const Text('破棄する'),
+                            ),
+                          ],
+                        ),
+                  ),
+              child: const Text('日記を破棄する'),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Text('作成'),
+        onPressed: () async {
+          print(_controller.text);
+          print(_text);
+          print(selectedDate);
+          print(stress);
+          if (stress is int) {
+            print('stress はint型です');
+          }
+          ;
+          Navigator.of(context).pop(_titleController.text);
+        },
+      ),
+    );
   }
+}
